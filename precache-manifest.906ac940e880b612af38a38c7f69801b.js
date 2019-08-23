de62110ced330bbc6274cb44bd1fba3bc5a63cfd
Ivan Rodriguez <ivanr.ibarra92@gmail.com>
2019-08-23 14:10:59 -0700

    Updates

---
+++ precache-manifest.906ac940e880b612af38a38c7f69801b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b48ae4c5b8a5d0255a25d82694f8c6",
+    "url": "/gamma/index.html"
+  },
+  {
+    "revision": "d2bc0a18a29b6ff5bc6e",
+    "url": "/gamma/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "387a3946ddc635718325",
+    "url": "/gamma/static/js/2.37ab121f.chunk.js"
+  },
+  {
+    "revision": "d2bc0a18a29b6ff5bc6e",
+    "url": "/gamma/static/js/main.a998eeca.chunk.js"
+  },
+  {
+    "revision": "fc7afc488afeee95b9ec",
+    "url": "/gamma/static/js/runtime~main.cc635432.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gamma/static/media/logo.5d5d9eef.svg"
+  }
+]);
